8986a04230cf6fdf8b55edbf62d68d434f4fe12a
Николай Хван <khvannickolay@gmail.com>
2023-08-31 23:50:44 +0500

    Diplom, try4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -213,11 +213,11 @@ class RecipeCUDSerializer(RecipeInFollowSerializer):
         )
 
     def to_representation(self, instance):
-        user = self.request.user
-        instance.is_favorited = user.favorites.filter(recipe=instance).exists()
-        instance.is_in_shopping_cart = user.shopping_carts.filter(
-            recipe=instance
-        ).exists()
+        # user = self.request.user
+        # instance.is_favorited = user.favorites.filter(recipe=instance).exists()
+        # instance.is_in_shopping_cart = user.shopping_carts.filter(
+        #     recipe=instance
+        # ).exists()
         return RecipeSerializer(instance, context=self.context).data
 
 
